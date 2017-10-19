8ab360cb182fd8464193373c5737c04474fe3613
Marc Schroth <marc.schroth@Marcs-MacBook-Pro-2.local>
2017-10-18 21:47:22 -0700

    Switch to gulp 4.0

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,22 @@
 
 import gulp from 'gulp';
 
-gulp.task('watch', ['todo']);
-
-// Build the "dist" folder for css, fonts, images, and javascript
-gulp.task('default', ['todo']);
-
-gulp.task('todo', function() {
+// Placeholder task, will be removed once real tasks are in place
+gulp.task('todo', () => {
   console.log("TODO");
   return true;
 });
+
+// Build the "dist" folder for css, fonts, images, and javascript
+gulp.task('build', gulp.series('todo'));
+
+// If given no command, run the build task
+gulp.task('default', gulp.series('build'));
+
+// Watch for changes to files. When a change is detected, re-run specific build tasks based on the file type.
+gulp.task('watch', () => {
+  gulp.watch('node_modules/**/*').on('all', ['build']);
+});
+
+// Build and then watch for changes to source files
+gulp.task('build:watch', gulp.series('build', 'watch'));
