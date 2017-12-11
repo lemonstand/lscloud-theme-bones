4c1b1c65b7f8702c0e9c16bfab5df22237a6a078
Marc Schroth <marc.schroth@Marcs-MBP-2.hitronhub.home>
2017-12-11 15:54:00 -0800

    If theme setting enabled and there is only one shipping option, auto-select it and submit the form to continue on to review/pay step of checkout. On manual navigation back to Shipping Option step, do not auto-submit.

--- resources/javascript/src/app.js
+++ resources/javascript/src/app.js
@@ -1,6 +1,7 @@
 import './lib/foundation-setup';
 
 import './lib/data-mirror';
+import './lib/data-shipping-method-auto-submit';
 import './lib/data-sort-redirect';
 import './lib/product-ajax';
 import './lib/shipping-options';
